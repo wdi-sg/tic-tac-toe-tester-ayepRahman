204cb9fdc049932484a7776317e1bee0bcf7ac73
arif rahman <matrep88@hotmail.com>
2017-03-19 16:24:58 +0800

    new test

---
+++ testing-the-tester.js
@@ -0,0 +1,28 @@
+var box = ['x', 'x', 'x',
+           'x', 'x', 'x',
+           'x', 'x', 'x']
+
+var playerInput = []
+
+
+function restart() {
+
+}
+
+function isGameOver() {
+  return false
+}
+
+function whoWon() {
+  return 0
+}
+
+function playTurn(index) {
+  var newMove = []
+
+  if(index === box[index]) {
+    return false
+  }
+
+  //return true
+}
