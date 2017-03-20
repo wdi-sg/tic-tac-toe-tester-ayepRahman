68d8009839f8656b565183fd73535a6b192c8dbe
arif rahman <matrep88@hotmail.com>
2017-03-20 10:19:02 +0800

    practice

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -19,17 +19,23 @@ function whoWon() {
 var box = ['x', 'x', 'x',
            'x', 'x', 'x',
            'x', 'x', 'x']
-var turn = ''
+var player = 1
 
+//input: index
+//sideEffectL swithc the variable player
+//output: boolean (t/f)
 
 function playTurn(index) {
 
   if (box[index]) {
     return false
    } else {
-     box[index] = turn
-     if (turn === '' )
-     turn = ''
+     box[index] = player
+     if(player === 1){
+     player = 2 //become player 2
      return true
+   } else {
+     player = 1
    }
-}
+ }
+ }
